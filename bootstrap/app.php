a2ed6af4bb826230e5a41b48cfb29067d66ccdb9
zahid2362 <zahid.a@infodigita.com>
2024-08-19 08:06:38 +0600

    fix: add api version

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Route;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -7,9 +8,13 @@ use Illuminate\Foundation\Configuration\Middleware;
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
-        api: __DIR__.'/../routes/api.v1.php',
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
+        then: function(){
+            return Route::middleware('api')
+            ->prefix('api/v1')
+            ->group(base_path('routes/api.v1.php'));
+        }
     )
     ->withMiddleware(function (Middleware $middleware) {
         //
